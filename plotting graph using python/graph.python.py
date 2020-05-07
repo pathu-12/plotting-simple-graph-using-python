f5d2f33a5951c755572fe18a46e3ff41a6cdd998
Prathamesh Khochade <64797897+pathu-12@users.noreply.github.com>
2020-05-07 22:31:18 +0530

    Add files via upload

---
+++ plotting graph using python/graph.python.py
@@ -0,0 +1,10 @@
+
+import matplotlib.pyplot as plt
+import math
+
+x=[1,2,3,4,5]
+y=[100,200,1000,500,700]
+plt.ylabel("sales of the electronics")
+plt.bar(x,y)
+plt.title("chart showing the electronics sale in india")
+plt.show()
